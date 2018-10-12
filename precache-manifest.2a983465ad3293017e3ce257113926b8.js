15caab7482c5f4a49b4086c8445e3a4c59f83955
lubomir <lubomirkavetskiy@gmail.com>
2018-10-12 11:19:23 +0300

    Updates

---
+++ precache-manifest.2a983465ad3293017e3ce257113926b8.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "66f28e11b4b10d1a02f0c8b85677264c",
+    "url": "/music-dj-controller/static/media/dj_girl.66f28e11.jpg"
+  },
+  {
+    "revision": "e273d1ffe46642d5ea932fba2ed2dabf",
+    "url": "/music-dj-controller/static/media/bg_image.e273d1ff.jpg"
+  },
+  {
+    "revision": "fcd7e0dec54119dcae79d24b566f6d72",
+    "url": "/music-dj-controller/static/media/MuseoSans.fcd7e0de.ttf"
+  },
+  {
+    "revision": "b3f14903c271aeb18ec5",
+    "url": "/music-dj-controller/static/js/runtime~main.b3f14903.js"
+  },
+  {
+    "revision": "5a1b9b49ce13900cff12",
+    "url": "/music-dj-controller/static/js/main.5a1b9b49.chunk.js"
+  },
+  {
+    "revision": "e9c20d25314ab4692be6",
+    "url": "/music-dj-controller/static/js/1.e9c20d25.chunk.js"
+  },
+  {
+    "revision": "5a1b9b49ce13900cff12",
+    "url": "/music-dj-controller/static/css/main.e9717fc5.chunk.css"
+  },
+  {
+    "revision": "e9c20d25314ab4692be6",
+    "url": "/music-dj-controller/static/css/1.f4289910.chunk.css"
+  },
+  {
+    "revision": "63e4f373fb173dfe346dd3351a3e2308",
+    "url": "/music-dj-controller/index.html"
+  }
+];
